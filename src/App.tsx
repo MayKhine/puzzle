82a10be67364e690eefcf45deb6bd99387e28eb5
May <maykhine.ada@gmail.com>
2023-08-16 17:42:05 -0400

    work on helper functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,37 @@
-const DIM = 40;
-import { useRef, useState } from "react";
-import { Square } from "./assets/components/Square";
-import { motion } from "framer-motion";
+const DIM = 40
+import { useRef, useState } from "react"
+import { Square } from "./assets/components/Square"
+import { motion } from "framer-motion"
+import { PuzzlePeice } from "./assets/components/PuzzlePeice"
 function App() {
-  const [xy, setXy] = useState([]);
-  const constraintsRef = useRef(null);
+  const constraintsRef = useRef(null)
 
   const createBox = (xMax: number, yMax: number) => {
-    const result = [];
+    const result = []
     for (let x = 0; x < xMax; x++) {
       for (let y = 0; y < yMax; y++) {
-        result.push(<Square x={x} y={y} dimension={DIM}></Square>);
+        result.push(<Square x={x} y={y} dimension={DIM}></Square>)
       }
     }
-    return result;
-  };
+    return result
+  }
 
   return (
     <div
       style={{ backgroundColor: "lightgray", height: "100%", width: "100%" }}
     >
-      Puzzle
       <motion.div ref={constraintsRef} style={puzzleConstraintStyle}>
         {createBox(2, 2)}
-        <motion.div
-          drag
-          dragSnapToOrigin
-          animate={{ top: xy[1], left: xy[0] }}
-          // animate={{ x: xy[0], y: xy[1] }}
-          onDragStart={() => {}}
-          onDragEnd={(event, info) => {
-            console.log("event: ", event, ", info: ", info);
-            setXy((xy) => [100 + (xy[0] ?? 0), 100 + (xy[1] ?? 0)]);
-          }}
-          dragConstraints={constraintsRef}
-          style={{
-            // top: xy[1],
-            // left: xy[0],
-            backgroundColor: "red",
-            width: DIM,
-            height: DIM,
-            zIndex: 1,
-            position: "absolute",
-          }}
+        <PuzzlePeice
+          xIndex={10}
+          yIndex={5}
+          dim={DIM}
+          xOffset={20}
+          yOffset={20}
         />
       </motion.div>
     </div>
-  );
+  )
 }
 
 const puzzleConstraintStyle = {
@@ -55,13 +40,14 @@ const puzzleConstraintStyle = {
   height: "500px",
   margin: "20px",
   position: "relative",
-};
+}
+
 const puzzlePieceStyle = {
   backgroundColor: "red",
   width: DIM,
   height: DIM,
   // position: "relative",
   // position: "absolute",
-};
+}
 
-export default App;
+export default App
--- src/assets/components/Square.tsx
+++ src/assets/components/Square.tsx
@@ -1,36 +1,23 @@
-import { FC } from "react";
+import { FC } from "react"
 
 export type SquareProps = {
-  x: number;
-  y: number;
-  dimension: number;
-};
+  x: number
+  y: number
+  dimension: number
+}
 
-export const Square: FC<SquareProps> = ({ x, y, dimension }) =>
-  // x: number,
-  // y: number,
-  // diameter: number
-  //   color: string,
-  //   border: string,
-  //   borderColor: string
-
-  {
-    return (
-      <div
-        style={{
-          position: "absolute",
-          top: x * dimension,
-          left: y * dimension,
-          width: dimension,
-          height: dimension,
-
-          //   top: 0 * 20,
-          //   left: 0 * 20,
-          //   width: dimension,
-          //   height: dimension,
-          backgroundColor: "blue",
-          border: "2px solid white",
-        }}
-      />
-    );
-  };
+export const Square: FC<SquareProps> = ({ x, y, dimension }) => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: x * dimension,
+        left: y * dimension,
+        width: dimension,
+        height: dimension,
+        backgroundColor: "blue",
+        border: "0px solid white",
+      }}
+    />
+  )
+}
